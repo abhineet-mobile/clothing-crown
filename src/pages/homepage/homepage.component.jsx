7a7fd661d1f9ad1d700d423248d9bcb6f0ab6b51
abhineet-mobile <avi.program@gmail.com>
2020-04-04 15:59:05 +0530

    broken our home page into smaller components and finshed styling of menu item components

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react' ; 
+import '../../Components/directory/directory.component';
+import './homepage.styles.scss' ;
+import Directory from '../../Components/directory/directory.component';
+
+const HomePage = ()  =>(
+<div className = 'homepage'>
+    <Directory/>
+
+</div>
+
+
+); 
+
+export default HomePage; 
