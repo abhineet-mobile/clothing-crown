facba28ddb7bb3e351d6e59e69a7470286a9b4cc
abhineet-mobile <avi.program@gmail.com>
2020-04-08 23:46:12 +0530

    created the shop item page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
 import { Switch, Route} from 'react-router-dom';
-
+import ShopPage from './pages/shop/shop.component'; 
 
 import './App.css';
-const HatsPage =()=>(
-  <div>
-    <h1> hats page </h1>
-
-  </div>
-);
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component ={HomePage}/>
-        <Route exact path = '/hats' component ={HatsPage} />
+        <Route exact path = '/Shop' component ={ShopPage} />
       </Switch>
     </div>
   );
