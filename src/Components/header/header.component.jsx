cc777ed6798a1030a64be1c1e4a21b556f0db572
abhineet-mobile <avi.program@gmail.com>
2020-04-26 14:27:41 +0530

    integrte Firebase created signin page with signin with google

--- src/Components/header/header.component.jsx
+++ src/Components/header/header.component.jsx
@@ -1,10 +1,11 @@
 import React from 'react'; 
 import {ReactComponent as Logo }  from '../../assets/crown.svg';
 import {Link} from 'react-router-dom';
+import {auth} from '../../firebase/firebase.utils'; 
 import './header.styles.scss'; 
 
 
-const Header =()=>(
+const Header =({currentUser})=>(
     <div className='header'>
         <Link  className='logo-container' to='/'>
             <Logo className='logo'/>
@@ -16,6 +17,11 @@ const Header =()=>(
         <Link className ='option' to='/contact'>
             CONTACT
         </Link>
+        {currentUser ?(
+            <div className='option' onClick= {()=> auth.signOut()}>Sign Out</div>
+            ):(
+            <Link className='option' to ='/signup'>Sign In</Link>
+            )}
         </div>
     </div>
     )
