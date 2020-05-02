374b417714cc810e5c8585c20d2a32ec047ee351
abhineet-mobile <avi.program@gmail.com>
2020-05-02 20:47:41 +0530

    updated shop data with new data, updated cart utils to add data in the cart

--- src/Components/custom-button/custom-button.component.jsx
+++ src/Components/custom-button/custom-button.component.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 import './custom-button.styles.scss'; 
 
-const CustomButton = ({children , isGoogleSigIn, ...otherProps})=>(
+const CustomButton = ({children , isGoogleSigIn, inverted, ...otherProps})=>(
 
-    <button className={`${isGoogleSigIn ? 'is-google-sign-in' : ''} custom-button`} {...otherProps}>
+    <button className={`${inverted ? 'inverted' : ''} custom-button`} {...otherProps}>
         {children}
     </button>
 )
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,6 +1,8 @@
 import cartActionTypes from './cart.types'; 
+import addItemsToCart from './cart.utils'; 
 const INITIAL_STATE = {
-    hidden: true
+    hidden: true, 
+    cartItems: []
 };
 
 const cartReducer = (state = INITIAL_STATE, action ) =>{
@@ -11,6 +13,11 @@ const cartReducer = (state = INITIAL_STATE, action ) =>{
                                     hidden: !state.hidden
 
                                 }
+        case cartActionTypes.ADD_ITEM:
+            return{
+                ...state,
+                cartItems: addItemsToCart(state.cartItems , action.payload)
+            }
         default:
         return state; 
 
